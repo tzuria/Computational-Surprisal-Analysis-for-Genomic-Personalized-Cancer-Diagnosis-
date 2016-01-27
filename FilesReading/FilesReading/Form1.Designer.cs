41972bad5b6f73ede992876d899b95e4654186f6
dudiventura <dudiventura@gmail.com>
2016-01-27 23:38:30 +0200

    Converting Gene Expression Data to csv file

---
+++ FilesReading/FilesReading/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace FilesReading
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdBrowse = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmdBrowse
+            // 
+            this.cmdBrowse.Location = new System.Drawing.Point(155, 21);
+            this.cmdBrowse.Name = "cmdBrowse";
+            this.cmdBrowse.Size = new System.Drawing.Size(100, 44);
+            this.cmdBrowse.TabIndex = 1;
+            this.cmdBrowse.Text = "generate CSV";
+            this.cmdBrowse.UseVisualStyleBackColor = true;
+            this.cmdBrowse.Click += new System.EventHandler(this.cmdBrowse_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(25, 45);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(100, 20);
+            this.txtPath.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "file path:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdBrowse);
+            this.Controls.Add(this.txtPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button cmdBrowse;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FilesReading/FilesReading/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FilesReading
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdBrowse_Click(object sender, EventArgs e)
+        {
+            string[] lines = new string[79];
+            int i = 0;
+
+            var outPutFile = @"C:\ProgramOutput\mRNAtemp1.csv";
+            
+            string[] filePaths = Directory.GetFiles(txtPath.Text+ @"\");
+
+//            lines = System.IO.File.ReadAllLines(path);
+            foreach (string file in filePaths)
+            {
+                
+                    lines[i] = ParseFile(file);
+                    i++;
+            }
+
+            File.WriteAllLines(outPutFile, lines);
+            MessageBox.Show(string.Format("Your .CSV file was generated to : {0}", outPutFile));
+        }
+
+        private string ParseFile(string filePath)
+        {
+            string reader = "";
+            string patianteGenesLevel = "";
+            string geneExpressionPattern = "(\\S*)\\s*(?<geneCount>\\S*)";
+            FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+            using (StreamReader lineReader = new StreamReader(fileStream))
+            {
+                reader = lineReader.ReadLine();
+                while ((reader = lineReader.ReadLine()) != null)
+                {
+                    patianteGenesLevel += Regex.Match(reader, geneExpressionPattern).Groups["geneCount"].Value + ",";
+                }
+            }
+            return patianteGenesLevel.Substring(0,patianteGenesLevel.Length-1);
+        }
+    }
+}
